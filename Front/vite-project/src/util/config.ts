251c6aef5870f895cea3b2517722a01459488a35
RenanLH <renan_lh@outlook.com>
2026-02-13 15:11:07 -0300

    update server path

--- Front/vite-project/src/util/config.ts
+++ Front/vite-project/src/util/config.ts
@@ -1,5 +1,5 @@
-//export const API_BASE_URL = "https://plataforma.apoiomigrante.online/api"
-//export const BASE_URL = "https://plataforma.apoiomigrante.online/"
+export const API_BASE_URL = "https://plataforma.apoiomigrante.online/api"
+export const BASE_URL = "https://plataforma.apoiomigrante.online/"
 
-export const API_BASE_URL = "http://localhost:9875/api"
-export const BASE_URL = "http://localhost:9875/api"
+//export const API_BASE_URL = "http://localhost:9875/api"
+//export const BASE_URL = "http://localhost:9875/api"
